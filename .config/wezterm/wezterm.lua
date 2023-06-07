6d7362ad393304f19ca564b77ddd430b7c07e241
joshuahicks <joshua.hicks@dashhudson.com>
2023-06-07 13:15:13 -0300

    update wezterm config to use new lua structure

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,21 +1,27 @@
 local wezterm = require 'wezterm'
 
-return {
-    color_scheme              = 'Gruvbox Dark',
-    initial_cols              = 175,
-    initial_rows              = 40,
-    font_size                 = 15,
-    window_close_confirmation = 'NeverPrompt',
-    keys                      = {
-        {
-            key = 'w',
-            mods = 'CMD',
-            action = wezterm.action.CloseCurrentPane { confirm = true },
-        },
-        {
-            key = 'k',
-            mods = 'CMD',
-            action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
-        },
+local config = {}
+
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+
+config.color_scheme = 'GruvboxDark'
+config.initial_rows = 40
+config.initial_cols = 175
+config.font_size = 14
+config.window_close_confirmation = 'NeverPrompt'
+config.keys = {
+    {
+        key = 'w',
+        mods = 'CMD',
+        action = wezterm.action.CloseCurrentPane { confirm = true },
+    },
+    {
+        key = 'k',
+        mods = 'CMD',
+        action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
     },
 }
+
+return config
