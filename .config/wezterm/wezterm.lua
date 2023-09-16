a57dde89a214b24d10ea1c89fafb9aaffb38fd37
joshuahicks <joshuahicks9@gmail.com>
2023-09-16 15:47:03 -0300

    remove tab bar

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,9 +6,9 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
--- config.color_scheme = 'GruvboxDark'
 config.color_scheme = 'Fairyfloss'
 config.window_decorations = "RESIZE"
+config.enable_tab_bar = false
 config.initial_rows = 40
 config.initial_cols = 175
 config.font_size = 14
@@ -24,18 +24,13 @@ config.keys = {
         mods = 'CMD',
         action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
     },
+    {
+        key = 'a',
+        mods = 'CMD',
+        action = wezterm.action.ShowTabNavigator
+    },
 }
 config.window_frame = {
-  inactive_titlebar_bg = '#353535',
-  active_titlebar_bg = '#655F80',
-  inactive_titlebar_fg = '#cccccc',
-  active_titlebar_fg = '#ffffff',
-  inactive_titlebar_border_bottom = '#2b2042',
-  active_titlebar_border_bottom = '#2b2042',
-  button_fg = '#cccccc',
-  button_bg = '#2b2042',
-  button_hover_fg = '#ffffff',
-  button_hover_bg = '#3b3052',
   border_left_width = '0.5cell',
   border_right_width = '0.5cell',
   border_bottom_height = '0.25cell',
