b7dd6eab213a35867dca8da56d5f15f4db4a05d9
joshuahicks <joshua.hicks@dashhudson.com>
2024-04-15 12:35:27 -0300

    remove transparency

--- .config/nvim/lua/joshua/lazy/colourscheme.lua
+++ .config/nvim/lua/joshua/lazy/colourscheme.lua
@@ -1,11 +1,10 @@
 return {
-    "sainnhe/gruvbox-material",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.g.gruvbox_material_background = 'soft'
-        vim.g.gruvbox_material_better_performance = 1
-        vim.g.gruvbox_material_transparent_background = 1
-        vim.cmd('colorscheme gruvbox-material')
-    end
+	"sainnhe/gruvbox-material",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		vim.g.gruvbox_material_background = "soft"
+		vim.g.gruvbox_material_better_performance = 1
+		vim.cmd("colorscheme gruvbox-material")
+	end,
 }
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,52 +1,51 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = {}
 
 if wezterm.config_builder then
-    config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
-config.color_scheme = 'Gruvbox Material (Gogh)'
+config.color_scheme = "Gruvbox Material (Gogh)"
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.965
 config.enable_tab_bar = false
 config.initial_rows = 40
 config.initial_cols = 175
 config.font_size = 14
 config.colors = {
-    cursor_fg = 'black',
+	cursor_fg = "black",
 }
 config.keys = {
-    {
-        key = 'w',
-        mods = 'CMD',
-        action = wezterm.action.CloseCurrentPane { confirm = true },
-    },
-    {
-        key = 'k',
-        mods = 'CMD',
-        action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
-    },
-    {
-        key = 'a',
-        mods = 'CTRL',
-        action = wezterm.action.ShowTabNavigator
-    },
-    {
-        key = 'E',
-        mods = 'CTRL|SHIFT',
-        action = wezterm.action.PromptInputLine {
-            description = 'Enter new name for tab',
-            action = wezterm.action_callback(function(window, pane, line)
-                -- line will be `nil` if they hit escape without entering anything
-                -- An empty string if they just hit enter
-                -- Or the actual line of text they wrote
-                if line then
-                    window:active_tab():set_title(line)
-                end
-            end),
-        },
-    },
+	{
+		key = "w",
+		mods = "CMD",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+	{
+		key = "k",
+		mods = "CMD",
+		action = wezterm.action.ClearScrollback("ScrollbackAndViewport"),
+	},
+	{
+		key = "a",
+		mods = "CTRL",
+		action = wezterm.action.ShowTabNavigator,
+	},
+	{
+		key = "E",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
 }
 
 return config
