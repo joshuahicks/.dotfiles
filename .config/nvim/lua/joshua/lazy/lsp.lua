5aeb979a4a5d1b65e6b357be7e6cb22684ba98a9
joshuahicks <joshua.hicks@dashhudson.com>
2024-04-16 09:03:11 -0300

    disable pyright analysis

--- .config/nvim/lua/joshua/lazy/lsp.lua
+++ .config/nvim/lua/joshua/lazy/lsp.lua
@@ -45,6 +45,21 @@ return {
 					},
 				},
 			},
+			pyright = {
+				settings = {
+					pyright = {
+						-- Using Ruff's import organizer
+						disableOrganizeImports = true,
+					},
+					python = {
+						analysis = {
+							-- Ignore all files for analysis to exclusively use Ruff for linting
+							ignore = { "*" },
+						},
+					},
+				},
+			},
+			ruff_lsp = {},
 			tsserver = {
 				filetypes = {
 					"javascript",
