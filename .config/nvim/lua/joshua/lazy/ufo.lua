d9030db1682bd06ef729c039de42e6624fdc7c95
joshuahicks <joshua.hicks@dashhudson.com>
2024-05-18 10:49:13 -0300

    fix foldenable

--- .config/nvim/lua/joshua/lazy/ufo.lua
+++ .config/nvim/lua/joshua/lazy/ufo.lua
@@ -1,26 +1,26 @@
 return {
-    "kevinhwang91/nvim-ufo",
-    dependencies = "kevinhwang91/promise-async",
+	"kevinhwang91/nvim-ufo",
+	dependencies = "kevinhwang91/promise-async",
 
-    config = function()
-        vim.o.foldlevel = 99
-        vim.o.foldlevelstart = 99
-        vim.o.foldenabled = true
+	config = function()
+		vim.o.foldlevel = 99
+		vim.o.foldlevelstart = 99
+		vim.o.foldenable = true
 
-        vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-        vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+		vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+		vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
 
-        vim.keymap.set('n', 'zK', function()
-            local winid = require('ufo').peekFoldedLinesUnderCursor()
-            if not winid then
-                vim.lsp.buf.hover()
-            end
-        end, { desc = "Peek Fold" })
+		vim.keymap.set("n", "zK", function()
+			local winid = require("ufo").peekFoldedLinesUnderCursor()
+			if not winid then
+				vim.lsp.buf.hover()
+			end
+		end, { desc = "Peek Fold" })
 
-        require('ufo').setup({
-            provider_selector = function(bufnr, filetype, buftype)
-                return { 'lsp', 'indent' }
-            end
-        })
-    end
+		require("ufo").setup({
+			provider_selector = function(bufnr, filetype, buftype)
+				return { "lsp", "indent" }
+			end,
+		})
+	end,
 }
