3c71beb3deadd29c807b610a02e8535c1cfd5f6c
joshuahicks <joshua.hicks@dashhudson.com>
2024-09-29 17:10:17 -0300

    fix hammerspoon init.lua file

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,13 +1,11 @@
 -- SOURCE: https://konradstaniszewski.com/blog/windows-between-spaces
 
--- Set up the logger
 local log = hs.logger.new("WindowMover", "info")
 
--- Function to get spaceId by space name
 function getSpaceIdByName(spaceName)
 	local spaceNames = hs.spaces.missionControlSpaceNames()
 	for uuid, desktops in pairs(spaceNames) do
-		log.i("UUID: " .. uuid) -- Log the UUID
+		log.i("UUID: " .. uuid)
 		for index, name in pairs(desktops) do
 			log.i("Index: " .. index .. ", Name: " .. tostring(name)) -- Log the index and name
 			if name == spaceName then
@@ -20,7 +18,6 @@ function getSpaceIdByName(spaceName)
 	return nil
 end
 
--- Function to move focused window to a specific space
 function moveFocusedWindowToSpace(spaceNumber)
 	local spaceName = "Desktop " .. spaceNumber
 	log.i("Attempting to move window to " .. spaceName)
@@ -40,28 +37,32 @@ function moveFocusedWindowToSpace(spaceNumber)
 	end
 end
 
--- Function to cycle focus between apps on the current desktop space
 function cycleFocusOnCurrentSpace()
-	-- Get all windows on the current space, excluding hidden or minimized ones
 	local currentWindows = hs.window.filter
 		.new()
 		:setCurrentSpace(true)
 		:setDefaultFilter({ visible = true, currentSpace = true })
 		:getWindows()
+	local currentFocusedWindow = hs.window.focusedWindow()
 
-	-- If there are any windows in the current space, cycle focus to the next one
-	if #currentWindows > 0 then
-		currentWindows[1]:focus()
+	for i, win in ipairs(currentWindows) do
+		if win == currentFocusedWindow then
+			local nextIndex = (i % #currentWindows) + 1
+			log.i("Cycling focus to window: " .. currentWindows[nextIndex]:title())
+			currentWindows[nextIndex]:focus()
+			currentWindows[nextIndex]:raise()
+			break
+		end
 	end
 end
 
--- Bind keys cmd + shift + 1-6
-for i = 1, 6 do
-	hs.hotkey.bind({ "cmd", "shift" }, tostring(i), function()
-		log.i("Hotkey pressed: cmd + shift + " .. i)
+-- Change the window to a new workspace
+for i = 1, 5 do
+	hs.hotkey.bind({ "alt", "shift" }, tostring(i), function()
+		log.i("Hotkey pressed: alt + shift + " .. i)
 		moveFocusedWindowToSpace(i)
 	end)
 end
 
--- Bind the function to a custom hotkey, e.g., Ctrl + Alt + Tab
+-- Change window focus in the current workspace
 hs.hotkey.bind({ "ctrl" }, "`", cycleFocusOnCurrentSpace)
